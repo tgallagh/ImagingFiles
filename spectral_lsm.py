2be02aa0115c161db01b56fe9cf7accfc207978a
Tara Gallagher <tgallagh@uci.edu>
2020-01-21 16:16:49 -0800

    Update spectral_lsm.py

--- spectral_lsm.py
+++ spectral_lsm.py
@@ -1,11 +1,18 @@
+# make plots of zeiss LSM files containing spectral 32-channel images
+
 # libraries
 import os
 import tifffile as tiff
 import numpy as np
+import matplotlib.pyplot as plt   
 
-
- os.chdir("//LFD-Server/Files/Data/lfdguest1/Gallagher, Tara/PseudomonasSpectral/Pyocyanin_MOPS_pH_01-19-2020/Tara_Spectral_01-19-2020")
+wavelen=np.linspace(410,695,32)  # make list of wavelengths for channels 1-32
+os.chdir("//LFD-Server/Files/Data/lfdguest1/Gallagher, Tara/PseudomonasSpectral/Pyocyanin_MOPS_pH_01-19-2020/Tara_Spectral_01-19-2020")
 image_stack=tiff.imread(os.listdir()[0]) # read in first file in directory
 image_stack.shape # shape of file
 spec = np.sum(image_stack[0,0,:,:,:]) 
-spec1=np.sum(spec,1)  # contains sum intensity for each 
+spec1=np.sum(spec,1)  # contains sum intensity for each channel (1-32)
+plt.plot(wavelen,spec1)  
+
+
+
