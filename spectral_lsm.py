8d3b7200802fa04405902454f861731707ffdf14
Tara Gallagher <tgallagh@uci.edu>
2020-01-21 16:17:59 -0800

    Update spectral_lsm.py

--- spectral_lsm.py
+++ spectral_lsm.py
@@ -13,6 +13,6 @@ image_stack.shape # shape of file
 spec = np.sum(image_stack[0,0,:,:,:]) 
 spec1=np.sum(spec,1)  # contains sum intensity for each channel (1-32)
 plt.plot(wavelen,spec1)  
-
+plt.show()
 
 
